80dbdd873dd103150ddfd740d0d51a5bcfd830af
JorgeRivera94 <jrivera28901@gmail.com>
2024-10-10 22:17:24 -0400

    CreateFile() should handle the case where file exists

--- imu_driver/src/imu_driver.c
+++ imu_driver/src/imu_driver.c
@@ -19,7 +19,25 @@ void CreateFile() {
   file_ptr = open("imu_data.txt", "r");
   if (file_ptr) {
     // file already exists
-    // TODO read the file
+    char line[1024];
+    char last_line[1024];  // to not lose the last line
+
+    // if line has value, assign it to last_line
+    while (fgets(line, sizeof(line), file_ptr)) {
+      strcpy(last_line, line);
+    }
+
+    // close file in read mode
+    fclose(file_ptr);
+
+    // update the time position, velocity and acceleration values to those
+    // previously stored in "imu_data.txt".
+    scanf(last_line, "%f,%lf,%lf,%lf,%lf,%lf,%lf", &time, &p_x, &p_y, &v_x,
+          &v_y, &acc_x, &acc_y);
+
+    // add time required for supervisor to create a new imu_driver
+    time += 1.5;
+
     return;
   } else {
     // file did not exist
@@ -28,7 +46,4 @@ void CreateFile() {
 
     // write initial values TODO
   }
-
-  // close file
-  fclose(file_ptr);
 }
